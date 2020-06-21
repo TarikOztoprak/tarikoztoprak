4f940ad41d6f569b097893c716a7b0191693e6de
Muhammet Tarık ÖZTOPRAK <56087286+TarikOztoprak@users.noreply.github.com>
2020-06-21 14:20:10 +0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
-var kucuk = false;
+var kucuk = true;
 function kucult() {
     if (kucuk == false) {
         document.getElementById("navigator").style.minWidth = "0vw";
         document.getElementById("icons").style.visibility = "hidden";
-        document.getElementById("navigator").style.background = "#dddddd";
+        
         document.getElementById("butt").textContent = "→";
         document.getElementById("butt").style.float = "left";
       
@@ -13,7 +13,7 @@ function kucult() {
     {
         document.getElementById("navigator").style.minWidth = "5vw";
         document.getElementById("icons").style.visibility = "visible";
-        document.getElementById("navigator").style.background = "#dddddd";
+     
         document.getElementById("butt").textContent = "←";
         document.getElementById("butt").style.float = "right";
        
