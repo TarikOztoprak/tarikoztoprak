e1327adad145a96d050c978fa2fa1ae159d93bc4
Muhammet Tarık ÖZTOPRAK <56087286+TarikOztoprak@users.noreply.github.com>
2020-07-14 11:40:05 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,37 +4,27 @@ function Ara() {
 
 //'{ "blogNo":"0" , "blogBaslik":"Null" },' +
 var text = '{ "blogs" : [' +
-'{"blogNo":"1" , "blogBaslik":"İlk Blog ve Kaydırma Çubuğu(Scroll Bar) Kapatma","blogResim":"example.jpg",'+
-'"blogIcerik":"&emsp; Kendime bir blog sitesi yazarak frontend ve backend bilgilerimi geliştirebileceğimi düşündüm. İşe yaradı da!'+
-'Ama bloğuma yazacağım içeriği hiç düşünmemiştim. Bugün, öğrendiğim ve daha sonra unutmak istemediğim bazı püf noktalarını'+
-'yazmaya karar verdim ve bu ilk bloğumu yazmaya başladım. <br> <br>'+
-'&emsp; Bloğumunda gereksiz bir şekilde yatay kaydırma çubuğu'+
-'(scroll bar) bulunmaktaydı ve sitemi çirkin göstermekteydi. Meğer basit bir css koduyla çözülebilmekteymiş. kod: <br>'+
-'<div >overflow-x: hidden;</div> x ekseninde kaydırma çubuğuna kapatır.'+
-'<div >overflow-y: hidden;</div> y ekseninde kaydırma çubuğunu kapatır.'+
-'<div>overflow: hidden</div> yaparsak hem x hem de y yönünde kaydırma çubuğunu kapatır. <br>'+
-'&emsp; Bu kodu css body içine ekleyerek sorunumu çözdüm.",'+
-'"blogTarih":"21.06.2020"},'+
+'{"blogNo":"1" , "blogBaslik":" ",'+"blogResim":"example.jpg",'+
+'"blogIcerik":" ",'+
+'"blogTarih":" "},'+
 
-'{"blogNo":"2" , "blogBaslik":"Deneme ","blogResim":"example.jpg",'+
-'"blogIcerik":"adsasfafafafasf'+
-'dsfdsafdafdsafdsafdsafdafda",'+
-'"blogTarih":"22.06.2020"},'+
+'{"blogNo":"2" , "blogBaslik":"  ","blogResim":"example.jpg",'+
+'"blogIcerik":" ",'+
+'"blogTarih":" "},'+
 
-'{"blogNo":"3" , "blogBaslik":"Deneme2 ","blogResim":"example.jpg",'+
-'"blogIcerik":"adsasfafafafasf'+
-'dsfdsafdafdsafdsafdsafdafda",'+
-'"blogTarih":"25.06.2020"},'+
+'{"blogNo":"3" , "blogBaslik":" ","blogResim":"example.jpg",'+
+'"blogIcerik":" '+
+'"blogTarih":" "},'+
 
-'{"blogNo":"4" , "blogBaslik":"Deneme3 ","blogResim":"example.jpg",'+
-'"blogIcerik":"adsasfafafafasf'+
-'dsfdsafdafdsafdsafdsafdafda",'+
-'"blogTarih":"25.06.2020"},' +
+'{"blogNo":"4" , "blogBaslik":" ","blogResim":"example.jpg",'+
+'"blogIcerik":" '+
+' ",'+
+'"blogTarih":" "},' +
 
-'{"blogNo":"5" , "blogBaslik":"Deneme4 ","blogResim":"example.jpg",'+
-'"blogIcerik":"adsasfafafafasf'+
-'dsfdsafdafdsafdsafdsafdafda",'+
-'"blogTarih":"25.06.2020"}]}';
+'{"blogNo":"5" , "blogBaslik":" ","blogResim":"example.jpg",'+
+'"blogIcerik":" '+
+' ",'+
+'"blogTarih":" "}]}';
 
 
 var maxblogNu = 4;
