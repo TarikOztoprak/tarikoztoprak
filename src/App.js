792b5c43fae9d0d925b1a223145348fc5dd5fdda
Muhammet Tarık ÖZTOPRAK <56087286+TarikOztoprak@users.noreply.github.com>
2021-06-19 13:39:34 +0300

    update
    add projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import SideBar from "./SideBar.js"
-function App() {
-  return (
-    <div className="App">
-      <SideBar/>
-      Hi
-    </div>
-  );
+import cardCollection from "./picture/my-card-collection.png"
+import wob from "./picture/wob.png"
+import React from 'react';
+class App extends React.Component {
+  state = {
+    headerList:["My Card Collection","Kitapların Dünyası"],
+    imageList:[cardCollection, wob],
+    infoList:["Oyun kartı yapıp, toplayabildiğin bir web sitesi. React kullanılarak yapılmıştır.",
+     "Bu proje kitapları görüntüleyip yorum yapabildiğin bir websitesidir. Admin paneli bulunmaktadır ve buradan kitap eklenebilir, editlenebilir ve silinebilir. Kullanıcıların mesajlarını kaldırabilir. Kullanıcılar kitaplara yorumda bulunabilir. ASP.NET CORE MVC kullanılarak yapılmıştır."],
+    linkList:["https://my-card-collection.herokuapp.com/", "https://github.com/TarikOztoprak/Web-Programlama-Proje-Odevi"],
+    index: 0
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <SideBar/>
+        <div className="Projects">
+          <h1 style={{minWidth: '100%' , marginTop: '0', float:'left', backgroundColor:'lightblue'}}>Projeler</h1>
+          
+          <button style={{float:'left'}} className="Projects-button" onClick={() => this.state.index === 0 ? this.setState({index: this.state.headerList.length-1}) : this.setState({index: this.state.index -1})} >{"<"}</button>      
+          <button style={{float:'right'}} className="Projects-button" onClick={() => this.state.index === this.state.headerList.length-1 ? this.setState({index: 0}) : this.setState({index: this.state.index +1})} >{">"}</button>      
+
+          <h1>{this.state.headerList[this.state.index]}</h1> 
+          <a href={this.state.linkList[this.state.index]}>
+          <img className="Project-image" src={this.state.imageList[this.state.index]} alt="pic"></img>
+          </a>
+          <br/>
+          <p>{this.state.infoList[this.state.index]}</p>
+         
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
